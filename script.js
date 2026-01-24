b338ded4b094212a4fde76cb4761efc4b01f23aa
Yessi Kenney <yessikenney@gmail.com>
2026-01-24 12:00:39 -0800

    added new operator buttons and accompanying css. changed grid to make room for ops. added script for clear button. add click events for adding numbers to input value. still need: make operator buttons usable, connect buttons to the operating functions

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 //let userNum = userInput.value;
 let total = 0;
-let x = userInput.value;
-console.log(parseInt(x));
 
-function operate(){
+function operate(x,y){
 
-}
+    plusSign.addEventListener("click", () => add(x,y));
+    minusSign.addEventListener("click", () => subtract(x,y));
+    timesSign.addEventListener("click", () => multiply(x,y));
+    divideSign.addEventListener("click", () => {
+            
+            userInput.value += numValue;
+
+        })
+    }
+    /*
+if (plusSign === true){
+    add(x,y)
+} else if (minusSign === true){
+    subtract(x,y)
+} else if (timesSign === true){
+    multiply(x,y)
+} else if (divideSign === true){
+    divide(x,y)
+    */
+
 function add(x,y){
      return x + y;
 //return total += num;
@@ -38,10 +55,44 @@ canvas.style.setProperty('width', '633px');
 canvas.style.setProperty('border', 'solid 1px black');
 
 
+const operators = document.getElementById("operators");
+/* OPERATOR SYMBOLS */ 
+const plusSign = document.createElement("button");
+const minusSign = document.createElement("button");
+const timesSign = document.createElement("button");
+const divideSign = document.createElement("button");
+plusSign.textContent = "+";
+minusSign.textContent = "−";
+timesSign.textContent = "×";
+divideSign.textContent="÷";
+operators.appendChild(plusSign);
+operators.appendChild(minusSign);
+operators.appendChild(timesSign);
+operators.appendChild(divideSign);
+
+// clear button
+const enter = document.createElement("button");
+enter.id = "enterBtn";
+enter.textContent = "ENTER";
+operators.appendChild(enter);
+
+const clear = document.createElement("button");
+clear.id = "clearBtn";
+clear.textContent = "CLEAR";
+operators.prepend(clear);
+
+clear.addEventListener("click", () => {
+    userInput.value='';
+    });
+
+let square;
+let text = '0';
+userInput.value = parseInt(text);
+
 function addGrid(element, squares){
     element.style.setProperty('background-color', '#f5f5f5')
     for (let i=0; i < squares*squares; i++){
-        let square = document.createElement('button')
+        square = document.createElement('button')
         square.style.setProperty('border', '1px solid #4a5a72')
         square.style.setProperty('width',`calc(100%/${squares})`)
         square.style.setProperty('height',`calc(100%/${squares})`)
@@ -50,14 +101,21 @@ function addGrid(element, squares){
         square.style.setProperty('flex','1 1 1');
         square.style.setProperty('font-size', '33px')
         square.textContent = i+1;
+        square.addEventListener("click", () => {
+                userInput.value='';
+
+            let numValue = parseInt(i+1); 
+            userInput.value += numValue;
+
+        })
 
        // square.style.setProperty('border-right', '1px solid #ffffff')
        // square.style.setProperty('border-top', '1px solid #4a5a72')  
        // square.style.setProperty('border-bottom', '1px solid #ffffff')  
-        square.addEventListener('mouseover', () => {
+       /* square.addEventListener('mouseover', () => {
         const rndCol = `rgb(${random(120)+80} ${random(115)} ${random(120)+80})`;
         square.style.backgroundColor = rndCol;     
-    })
+    })*/
             canvas.appendChild(square)
     
 };
